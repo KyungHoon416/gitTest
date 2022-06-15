811f614fa8d9233167a083240cc44081c75fd92d
kyunghoon <rudgns416416@cknb.co.kr>
2022-06-15 15:44:33 +0900

    gitTest

---
+++ gitTEST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitTEST
+//
+//  Created by Inje Cho on 2022/06/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Git 테스트")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gitTEST/gitTESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTESTApp.swift
+//  gitTEST
+//
+//  Created by Inje Cho on 2022/06/15.
+//
+
+import SwiftUI
+
+@main
+struct gitTESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
